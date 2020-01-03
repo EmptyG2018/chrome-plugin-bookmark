39d371bc89bca2fe166440b2c7cf1ac9cd393d19
EmptyG2018 <Empty.G.2018@Gmail.com>
2020-01-03 23:39:43 +0800

    新增登录请求API(未完成)

---
+++ config.js
@@ -0,0 +1,3 @@
+var config = {
+	url: 'localhost:1234'
+}
--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,37 @@
-document.body="";
+$("#login-sub").on("click", function(){
+	var loginEmailVal = $("#login-input-email").val();
+	var loginPwdVal = $("#login-input-pwd").val();
+	if(!$.trim(loginEmailVal)) { console.log('邮箱不能为空'); return; }
+	if(!$.trim(loginPwdVal)) { console.log('密码不能为空'); return; }
+	// $.ajax({
+	// 	url: config.url+'/api/v1/bookmark/user/getToken',
+	// 	type: 'post',
+	// 	data: {
+	// 		email: loginEmailVal,
+	// 		pwd: loginPwdVal
+	// 	},
+	// 	success: function(res) {
+	// 		console.log(res);
+	// 	},
+	// 	error: function(err) {
+	// 		console.log(err);
+	// 	}
+	// })
+	$.ajax({
+		url: 'https://admin.punctualcat.com/oauth/token',
+		type: 'post',
+		data: {
+			client_id: '100008',
+			client_secret: 'oBAeJKw36sG6ozStfNkhttHrwrVG6L7tYRN55T2T5qKsEhlmCcgzHRcYhhWfJyNP',
+			grant_type: 'password',
+			username: '17688723510',
+			password: '123456'
+		},
+		success: function(res){
+			console(res);
+		},
+		error: function(err){
+			console(err);
+		}
+	})
+})
