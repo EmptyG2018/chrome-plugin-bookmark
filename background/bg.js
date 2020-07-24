a533bba2530b90419ed4375fff68cc2963a52bd9
EmptyG2018 <Empty.G.2018@Gmail.com>
2020-07-24 18:30:29 +0800

    Jquery框架转为vue框架

--- background/bg.js
+++ background/bg.js
@@ -1,15 +1,7 @@
-
-
 // 创建右键菜单
-chrome.contextMenus.create({type: "normal", contexts: ["page"] , onclick: function(info, tab){
-		console.log(info); console.log(tab)
-		
+chrome.contextMenus.create({type: "normal", contexts: ["page"] , onclick: function(info, tab){	
 		// 创建新so选项卡
-		chrome.tabs.create({url: "chrome://newtab"},function(){
-			console.log("创建新so选项卡成功");
-		})
+		chrome.tabs.create({url: "chrome://newtab"})
 	}, 
-	title: "新建so选项卡" }, function(){
-		console.log("创建右键菜单成功");
-	}
+	title: "新建so选项卡" }
 )
