9d7f5315c9820a5a7fef229e15ec287ac74b2dc9
EmptyG2018 <Empty.G.2018@Gmail.com>
2020-07-27 16:39:53 +0800

    新增bookmark右侧弹窗动画

--- content/content.js
+++ content/content.js
@@ -1,33 +1,39 @@
 var html = "";
-html+="<div class='fly-bookmark'>";
-	html+="<div class='fly-bookmark-main'>"
-		html+="<div class='fly-bookmark-search'>";
-			html+="<div class='form-item-input'>";
-				html+="<input type='text' placeholder='请输入标题，或url地址' />";
-			html+="</div>";
-		html+="</div>"
-		html+="<div class='fly-bookmark-list'>";
-			html+="<div class='fly-bookmark-item' v-for='bookmark in bookmarks' :key='bookmark.id'>";
-				html+="<div class='fly-bookmark-cell'>";
-					html+="<div class='fly-bookmark-tit'>{{bookmark.tit}}</div>";
-					html+="<div class='fly-bookmark-url'>{{bookmark.url}}</div>";
+html+=`<div id='fly-app' class='fly-app' :class="{'slide-in': show }">`;
+	html+="<div class='fly-bookmark'>";
+		html+="<div class='fly-bookmark-main'>"
+			html+="<div class='fly-bookmark-search'>";
+				html+="<div class='form-item-input'>";
+					html+="<input type='text' placeholder='请输入标题，或url地址' />";
+				html+="</div>";
+			html+="</div>"
+			html+="<div class='fly-bookmark-list'>";
+				html+="<div class='fly-bookmark-item' v-for='bookmark in bookmarks' :key='bookmark.id'>";
+					html+="<div class='fly-bookmark-cell'>";
+						html+="<div class='fly-bookmark-tit'>{{bookmark.tit}}</div>";
+						html+="<div class='fly-bookmark-url'>{{bookmark.url}}</div>";
+					html+="</div>";
 				html+="</div>";
 			html+="</div>";
 		html+="</div>";
 	html+="</div>";
+	html+="<div class='fly-toggle' @click='appToggle'></div>";
 html+="</div>";
-html+="<div class='fly-toggle'></div>";
 
 var body = document.getElementsByTagName("body")[0];
 var appEl = document.createElement("div");
-appEl.id = "fly-app";
-appEl.className = "fly-app";
+appEl.id = "fly-space";
+appEl.className = "fly-space";
 appEl.innerHTML = html;
 body.appendChild(appEl);
 
 const bookmark = new Vue({
   el: "#fly-app",
   data: {
+		// app展示状态
+		show: false,
+
+		// 书签模拟数据
     bookmarks: [
 			{ id: 1, tit: "百度一下，你就知道", url: "https://www.baidu.com" },
 			{ id: 2, tit: "element_百度搜索", url: "https://www.baidu.com/s?ie=utf-8&f=3&rsv_bp=1&tn=baidu&wd=element&oq=z-index%25E6%259C%2580%25E5%25A4%25A7%25E5%2580%25BC&rsv_pq=981e49a300031cab&rsv_t=584eRSLJaGWBqIIjQjUHhTHRRpzozzLanFEqbjIicrW%2B97BJ%2FYHLE5trmFY&rqlang=cn&rsv_enter=1&rsv_dl=ts_0&rsv_sug3=5&rsv_sug1=5&rsv_sug7=100&rsv_sug2=0&rsv_btype=t&prefixsug=elem&rsp=0&inputT=2955&rsv_sug4=2956" },
@@ -36,5 +42,10 @@ const bookmark = new Vue({
 			{ id: 5, tit: "天猫tmall.com--理想生活上天猫", url: "https://www.tmall.com/" },
 			{ id: 6, tit: "腾讯首页", url: "https://www.qq.com/" }
 		]
-  }
+  },
+	methods: {
+		appToggle() {
+			this.show = !this.show;
+		}
+	}
 })
